080a347f57f48ea290403426ef594685b4c497f0
Maria Isabel Gonzalez <isabel_133@hotmail.com>
2022-05-24 12:18:56 -0500

    created a function that lists the items in the basket array

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -10,5 +10,18 @@ function addCart(newItem){
         }
 console.log('We added an item to the cart, so it should be true:', addCart());
 
+let list;
+function listItems(list){
+    console.log('The list of items include:', list)
+    for(i=0;i<basket.length; i++);
+    basket.push(list)
+    return basket
+}
 
+listItems('chocolate');
+listItems('bread');
+listItems('milk');
+listItems('butter');
 
+function empty(reset){
+  
