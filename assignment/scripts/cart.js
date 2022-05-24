0f933b25670028c83d3a2182c046aec63d9fbeb1
Maria Isabel Gonzalez <isabel_133@hotmail.com>
2022-05-24 12:00:11 -0500

    created a function for adding items

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -3,3 +3,12 @@ console.log('***** Cart Functions *****');
 // We want to see how you are testing your code!!!
 
 let basket=[];
+
+function addCart(newItem){
+    if(newItem=basket.push('item'));
+            return true
+        }
+console.log('We added an item to the cart, so it should be true:', addCart());
+
+
+
