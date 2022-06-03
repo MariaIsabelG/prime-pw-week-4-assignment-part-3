85be2a67b721568bf4b7a82d993e11a8569edc85
Maria Isabel Gonzalez <isabel_133@hotmail.com>
2022-06-03 11:47:32 -0500

    trying to fix logging items in array

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -5,22 +5,23 @@ console.log('***** Cart Functions *****');
 let basket=[];
 
 function addCart(newItem){
-    newItem=basket.push('shampoo');
-    if(basket[0]==='shampoo'){
+    basket.push(newItem);
+    if(basket[0]===newItem){
         return true
     }else{
         return false
     }
 }
-console.log('Added shampoo, so it should be true:', addCart(basket))
+console.log('Added shampoo, so it should be true:', addCart('shampoo'))
 
 
 let list;
-function listItems(list){
+ function listItems(list){
     console.log('The list of items include:', list)
-    for(i=0;i<basket.length; i++);
+    for(let i=0;i<basket.length; i++){
     basket.push(list)
     return basket
+    }
 }
 
 listItems('chocolate');
@@ -29,9 +30,10 @@ listItems('milk');
 listItems('butter');
 
 
-function empty(anyArray){
-        anyArray.length=0
-        return anyArray
-    }
-console.log('Emptied the basket:', empty(basket));
+
+//function empty(anyArray){
+//        anyArray.length=0
+ //       return anyArray
+ //   }
+//console.log('Emptied the basket:', empty(basket));
 
